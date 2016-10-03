a2be75d18d6bd95ce66c348e61f1410568ce4ff7
chuckwalla <jeelthompson@gmail.com>
2016-10-02 17:57:24 -0700

    moved droplet id to secrets.yaml

--- restart_sudochuckwalla.rb
+++ restart_sudochuckwalla.rb
@@ -3,7 +3,8 @@ require 'droplet_kit'
 
 my_secrets = YAML::load_file('../../secrets.yaml') 
 token = my_secrets["digital_ocean"]["PAT"]
+droplet_id = my_secrets["digital_ocean"]["sudochuckwalla_droplet_id"]
 
 client = DropletKit::Client.new(access_token: token)
 
-client.droplet_actions.reboot(droplet_id: 24664322)
+client.droplet_actions.reboot(droplet_id: droplet_id)
